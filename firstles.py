4d3dd0f82f48132103ba079a5349ed981ff4fe95
sl1116 <vum0227@gmail.com>
2021-10-23 17:41:06 +0700

    commit

---
+++ firstles.py
@@ -0,0 +1,10 @@
+car = "subaru"
+print("Is car == 'subaru'? I predict true")
+print(car == "subaru")
+print("")
+print("Is car == 'ferari'? I predict False")
+print(car == "ferari")
+print("")
+members = ["a", "b", "c"]
+letter = "a"
+print(letter in members)
